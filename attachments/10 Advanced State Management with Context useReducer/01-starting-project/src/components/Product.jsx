7f21b163abf4cf3e523dedee8352b2438933d77e
Hector Jarquin <hectorjarquin@outlook.com>
2025-03-11 15:53:47 -0400

    Store - Shopping cart context - Add addItemToCart empty function. App - Deconstruct context values. Components - Product - Add addItemToCart context.

--- attachments/10 Advanced State Management with Context useReducer/01-starting-project/src/components/Product.jsx
+++ attachments/10 Advanced State Management with Context useReducer/01-starting-project/src/components/Product.jsx
@@ -1,12 +1,17 @@
+import { useContext } from 'react';
+import { CartContext } from '../store/shopping-cart-context.jsx';
+
 export default function Product({
   id,
   image,
   title,
   price,
   description,
-  onAddToCart,
 }) {
-  return (
+
+  const { addItemToCart } = useContext(CartContext);
+  
+  return ( 
     <article className="product">
       <img src={image} alt={title} />
       <div className="product-content">
@@ -16,7 +21,7 @@ export default function Product({
           <p>{description}</p>
         </div>
         <p className='product-actions'>
-          <button onClick={() => onAddToCart(id)}>Add to Cart</button>
+          <button onClick={() => addItemToCart(id)}>Add to Cart</button>
         </p>
       </div>
     </article>
